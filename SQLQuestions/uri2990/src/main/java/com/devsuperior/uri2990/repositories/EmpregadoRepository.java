6577481c5b612d63fe884cc9d9fde586a1345ae8
GiancarloJr <giancarlo456@hotmail.com>
2022-11-20 17:04:45 -0300

    Question 2990 Solved

--- SQLQuestions/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ SQLQuestions/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+
+        @Query(nativeQuery = true, value = "select empregados.cpf, empregados.enome,departamentos.dnome from empregados " +
+                "inner join departamentos ON empregados.dnumero = departamentos.dnumero " +
+                "where empregados.cpf not in (select empregados.cpf from empregados inner join trabalha ON trabalha.cpf_emp = empregados.cpf) " +
+                "order by empregados.cpf")
+        List<EmpregadoDeptProjection> search1();
 
 }
